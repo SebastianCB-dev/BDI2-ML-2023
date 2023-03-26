7a9a8b6f01f545cf70efb964c687297aeda6443e
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-26 08:49:44 -0500

    Add Driver validator

--- web_scraping/app.py
+++ web_scraping/app.py
@@ -3,9 +3,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
 
-service = Service('./drivers/chromedriver.exe')
+# Como saber el OS que estamos usando
+import platform
+platform = platform.system()
+service = None
+if platform == "Windows":  
+  service = Service('./drivers/chromedriver.exe')
+elif platform == "Linux":
+  service = Service('./drivers/chromedriver')
+elif platform == "Darwin":
+  service = Service('./drivers/chromedriver_mac')
+else:
+  Exception("No se reconoce el sistema operativo")
 driver = webdriver.Chrome(service=service)
-
 # abrir google.com
 driver.get("https://www.google.com")
 
