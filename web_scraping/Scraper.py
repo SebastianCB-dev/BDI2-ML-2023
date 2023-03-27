f21c62ba4ce8b8e5e2d7753ec4c42c291ecbdcf3
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-26 19:16:20 -0500

    Add Login and go to profile

--- web_scraping/Scraper.py
+++ web_scraping/Scraper.py
@@ -31,18 +31,46 @@ class Scraper:
 
   def getUsersFromInstagram(self):
     self.driver.get("https://www.instagram.com/")
-    # Login to Instagram
+    # Login to Instagram fielding username and password
     username_field = WebDriverWait(self.driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "username"))
+      EC.presence_of_element_located(
+          (By.NAME, "username")
+      )
     )
     username_field.send_keys(os.getenv("IG_USERNAME"))
     password_field = WebDriverWait(self.driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "password"))
+      EC.presence_of_element_located(
+        (By.NAME, "password")
+      )
     )
     password_field.send_keys(os.getenv("IG_PASSWORD"))
     # Click on login button
     login_button = WebDriverWait(self.driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+      EC.presence_of_element_located(
+        (By.XPATH, "//button[@type='submit']")
+      )
     )
     login_button.click()
+    # Click on not now button
+    not_now_button = WebDriverWait(self.driver, 10).until(
+      EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(text(), 'Not Now')]")
+      )
+    )
+    not_now_button.click()
+    # Go to profile
+    profile_button = WebDriverWait(self.driver, 10).until(
+      EC.presence_of_element_located(
+        (By.XPATH, "//a[contains(@href, '/" + os.getenv("IG_USERNAME") + "/')]")
+      )
+    )
+    profile_button.click()
+    # Go to followers
+    followers_button = WebDriverWait(self.driver, 10).until(
+      EC.presence_of_element_located(
+        (By.XPATH, "//a[contains(@href, '/" + os.getenv("IG_USERNAME") + "/followers')]")
+        )
+    )
+    followers_button.click()
+
     time.sleep(10)
