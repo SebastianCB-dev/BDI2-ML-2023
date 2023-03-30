0706203d564c900dc63838ee57b374e742260822
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-30 18:08:38 -0500

    Feat: Add Users Service DB Administrator

--- web_scraping/microservice_get_users/services/users.service.py
+++ web_scraping/microservice_get_users/services/users.service.py
@@ -1,4 +1,13 @@
+import psycopg2
 
 
 class UsersService:
-    pass
+  def __init__(self, host, port, database, user, password):
+    self.conn = psycopg2.connect(
+      host=host,
+      port=port,
+      database=database,
+      user=user,
+      password=password
+    )
+    self.cur = self.conn.cursor()
