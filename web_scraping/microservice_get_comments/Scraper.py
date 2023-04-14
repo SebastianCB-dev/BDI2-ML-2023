85beff988ed3f38abcac0a4a34e9e21a7085bc8a
SebastianCB <sebastian.carrillob01@gmail.com>
2023-04-13 19:10:11 -0500

    Feat: Add get posts from user

--- web_scraping/microservice_get_comments/Scraper.py
+++ web_scraping/microservice_get_comments/Scraper.py
@@ -53,7 +53,7 @@ class Scraper:
         else:
             self.logger.error("Don't recognize the operating system")
             Exception("Don't recognize the operating system")
-        if(platform == "Windows"):
+        if (platform == "Windows"):
             self.driver = webdriver.Chrome(executable_path=driver_path)
             return
         options = webdriver.ChromeOptions()
@@ -62,12 +62,13 @@ class Scraper:
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--incognito')
-        self.driver = webdriver.Chrome(executable_path=driver_path, options=options)
+        self.driver = webdriver.Chrome(
+            executable_path=driver_path, options=options)
 
     def getCommentsFromInstagram(self):
         self.driver.delete_all_cookies()
         # This function gets all users that the account is following from Instagram
-        self.driver.get("https://www.instagram.com/")
+        self.driver.get("https://www.instagram.com/accounts/login")
         # Login to Instagram fielding username and password
         username_field = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.NAME, "username"))
@@ -79,7 +80,8 @@ class Scraper:
         password_field.send_keys(os.getenv("IG_PASSWORD"))
         # Click on login button
         login_button = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+            EC.presence_of_element_located(
+                (By.XPATH, "//button[@type='submit']"))
         )
         login_button.click()
         # Click on not now button
@@ -94,9 +96,53 @@ class Scraper:
 
         while True:
             # TODO: Get From Database 10 in PENDING STATUS
-            users = []
-        
+            users = ['pamela.moreno', 'anabreco', 'san27ivette']
+            for user in users:
+                self.driver.get('https://www.instagram.com/' + user)
+                SCROLL_PAUSE_TIME = 1
+                posts_urls = []
+                while True:
+                    # Get scroll height
+                    # This is the difference. Moving this *inside* the loop
+                    # means that it checks if scrollTo is still scrolling
+                    last_height = self.driver.execute_script(
+                        "return document.body.scrollHeight")
+                    # print(last_height)
+                    # Scroll down to bottom
+                    self.driver.execute_script(
+                        "window.scrollTo(0, document.body.scrollHeight);")
 
+                    # Wait to load page
+                    time.sleep(SCROLL_PAUSE_TIME)
+
+                    # Calculate new scroll height and compare with last scroll height
+                    new_height = self.driver.execute_script(
+                        "return document.body.scrollHeight")
+                    posts_urls = self.driver.execute_script(
+                        "const posts_urls = []; document.querySelectorAll('div._aabd._aa8k._al3l').forEach(post => posts_urls.push('https://instagram.com' + post.querySelector('a').getAttribute('href'))); return posts_urls")
+                    if new_height == last_height:
+
+                        # try again (can be removed)
+                        self.driver.execute_script(
+                            "window.scrollTo(0, document.body.scrollHeight);")
+
+                        # Wait to load page
+                        time.sleep(SCROLL_PAUSE_TIME)
+
+                        # Calculate new scroll height and compare with last scroll height
+                        new_height = self.driver.execute_script(
+                            "return document.body.scrollHeight")
+
+                        # check if the page height has remained the same
+                        if new_height == last_height:
+                            # if so, you are done
+                            break
+                        # if not, move on to the next loop
+                        else:
+                            last_height = new_height
+                            continue
+                print(posts_urls)
+                
     def buscar_botones(self, driver, botones):
         for boton in botones:
             try:
