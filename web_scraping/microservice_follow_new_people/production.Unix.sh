b045e7d551916ab7945538532cf569cbb5ae06f1
SebastianCB <sebastian.carrillob01@gmail.com>
2023-04-27 17:28:13 -0500

    Feat: Add necessary files

---
+++ web_scraping/microservice_follow_new_people/production.Unix.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+DOCKER_MICROSERVICE_GET_USERS="microservice_get_users"
+
+DOCKER_VERSION_IMG="1.0.1-prod"
+
+# build browser
+docker build -t $DOCKER_MICROSERVICE_GET_USERS .
+
+# Push version
+docker tag $DOCKER_MICROSERVICE_GET_USERS sebastiancb/microservice_get_users:$DOCKER_VERSION_IMG
+
+docker push sebastiancb/microservice_get_users:$DOCKER_VERSION_IMG
