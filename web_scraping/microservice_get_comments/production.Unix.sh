49c4d4811f253453e34dffac92107134a2bd28d4
SebastianCB <sebastian.carrillob01@gmail.com>
2023-04-27 17:17:11 -0500

    Update production.Unix.sh

--- web_scraping/microservice_get_comments/production.Unix.sh
+++ web_scraping/microservice_get_comments/production.Unix.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-DOCKER_MICROSERVICE_GET_USERS="microservice_get_users"
+DOCKER_MICROSERVICE_GET_COMMENTS="microservice_get_comments"
 
 DOCKER_VERSION_IMG="1.0.1-prod"
 
 # build browser
-docker build -t $DOCKER_MICROSERVICE_GET_USERS .
+docker build -t $DOCKER_MICROSERVICE_GET_COMMENTS .
 
 # Push version
-docker tag $DOCKER_MICROSERVICE_GET_USERS sebastiancb/microservice_get_users:$DOCKER_VERSION_IMG
+docker tag $DOCKER_MICROSERVICE_GET_COMMETS sebastiancb/microservice_get_comments:$DOCKER_VERSION_IMG
 
-docker push sebastiancb/microservice_get_users:$DOCKER_VERSION_IMG
+docker push sebastiancb/microservice_get_comments:$DOCKER_VERSION_IMG
