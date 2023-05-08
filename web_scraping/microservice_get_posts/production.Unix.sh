690c152d73534d29be20030d18f057f8d05ba3dc
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-07 19:46:32 -0500

    Update production.Unix.sh with semillerosmart's name

--- web_scraping/microservice_get_posts/production.Unix.sh
+++ web_scraping/microservice_get_posts/production.Unix.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-DOCKER_MICROSERVICE_GET_COMMENTS="microservice_get_comments"
+DOCKER_MICROSERVICE_GET_POSTS="microservice_get_posts"
 
-DOCKER_VERSION_IMG="1.0.1-prod"
+DOCKER_VERSION_IMG="1.0.0-prod"
 
 # build browser
-docker build -t $DOCKER_MICROSERVICE_GET_COMMENTS .
+docker build -t $DOCKER_MICROSERVICE_GET_POSTS .
 
 # Push version
-docker tag $DOCKER_MICROSERVICE_GET_COMMETS sebastiancb/microservice_get_comments:$DOCKER_VERSION_IMG
+docker tag $DOCKER_MICROSERVICE_GET_POSTS semillerosmart/microservice_get_posts:$DOCKER_VERSION_IMG
 
-docker push sebastiancb/microservice_get_comments:$DOCKER_VERSION_IMG
+docker push semillerosmart/microservice_get_posts:$DOCKER_VERSION_IMG
