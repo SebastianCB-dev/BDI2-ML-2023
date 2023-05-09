084f042b81731203d0abab99cf0b472d32616730
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-09 18:35:49 -0500

    Fix: Name microservice get_comments

--- web_scraping/microservice_get_comments/production.Unix.sh
+++ web_scraping/microservice_get_comments/production.Unix.sh
@@ -6,9 +6,9 @@ DOCKER_MICROSERVICE_GET_COMMENTS="microservice_get_comments"
 DOCKER_VERSION_IMG="1.0.0-prod"
 
 # build browser
-docker build -t $DOCKER_MICROSERVICE_GET_COMMENTS .
+docker build --no-cache -t $DOCKER_MICROSERVICE_GET_COMMENTS .
 
 # Push version
-docker tag $DOCKER_MICROSERVICE_GET_COMMETS semillerosmart/microservice_get_comments:$DOCKER_VERSION_IMG
+docker tag $DOCKER_MICROSERVICE_GET_COMMENTS semillerosmart/microservice_get_comments:$DOCKER_VERSION_IMG
 
 docker push semillerosmart/microservice_get_comments:$DOCKER_VERSION_IMG
