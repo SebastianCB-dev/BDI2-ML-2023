58047d091ff000052202cd6777ce9e08df8150b9
SebastianCB <sebastian.carrillob01@gmail.com>
2023-05-10 20:07:54 -0500

    Feat: Update production.Unix.sh add no cache build

--- web_scraping/microservice_follow_new_people/production.Unix.sh
+++ web_scraping/microservice_follow_new_people/production.Unix.sh
@@ -6,7 +6,7 @@ DOCKER_MICROSERVICE_FOLLOW_NEW_PEOPLE="microservice_follow_new_people"
 DOCKER_VERSION_IMG="1.0.0-prod"
 
 # build browser
-docker build -t $DOCKER_MICROSERVICE_FOLLOW_NEW_PEOPLE .
+docker build --no-cache -t $DOCKER_MICROSERVICE_FOLLOW_NEW_PEOPLE .
 
 # Push version
 docker tag $DOCKER_MICROSERVICE_FOLLOW_NEW_PEOPLE semillerosmart/microservice_follow_new_people:$DOCKER_VERSION_IMG
