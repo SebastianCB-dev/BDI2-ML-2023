374f0b38826837e5bf3c7cfb302d303c774e2a7a
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-05-27 12:57:20 -0500

    Feat: Add Structure app.py

--- artificial_intelligence/app.py
+++ artificial_intelligence/app.py
@@ -2,21 +2,24 @@ from preprocessing_service import Preprocesamiento
 from model_word2vec_service import ModelWord2Vec
 import joblib
 
+# Load models
 pp = Preprocesamiento()
 w2v = ModelWord2Vec()
 clf = joblib.load('./models/logistic_regression.pkl')
 
-#comentario = "Me quiero suicidar este día no puede ser peor que el de ayer mi padre me odio y mi madre no me quiere ver"
-comentario = "Me quiero suicidar este día no puede ser peor que el de ayer mi padre me odio y mi madre no me quiere ver"
-# Preprocesado del comentario
-comentario_procesado = pp.preprocesamiento_con_ortografia(comentario)
-if comentario_procesado == "":
-  print("El comentario despúes del procesamiento no cuenta con la suficiente información para continuar con la impresión diagnostica")
-else:
-  #Obtener la similitud de coseno entre el comentario y 
-  #Cada una de las respuestas del inventario de depresión de BECK (BDI-II)
-  cosine_similarity_beck = w2v.get_cosine_similarity_BECK(comentario_procesado)
-  # Obtener la respuesta por item basandose en la similitud de coseno
-  results_beck = w2v.get_result_beck(cosine_similarity_beck)
-  print("El comentario lleno el inventario BECK de esta manera:", results_beck)
-  print('Predicción:', clf.predict([results_beck]))
+while True:
+  # Get 10 comments from database
+  comments = ['Estoy super feliz, quiero salir a pasear', 'Estoy muy triste, no quiero salir de mi casa']
+  for comment in comments:
+    # TODO: Identify if the comment is in Spanish or English    
+    # Preprocessing comment
+    comment_processed = pp.preprocesamiento_con_ortografia(comment)
+    if(comment_processed == ""):
+      # TODO: Change status of comment to "REVIEWED"
+      continue
+    # TODO: Check if exists user in database
+    print('Comment processed: ', comment_processed)
+    cosine_similarity_beck = w2v.get_cosine_similarity_BECK(comment_processed)
+    print('Cosine similarity BECK: ', cosine_similarity_beck)
+    results_beck = w2v.get_result_beck(cosine_similarity_beck)
+    print('Results BECK: ', results_beck)
