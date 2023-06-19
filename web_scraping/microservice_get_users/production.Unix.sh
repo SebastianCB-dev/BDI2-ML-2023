86f780c6fdabdc44491a1f630c76f53d864a28e2
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-06-19 14:01:53 -0500

    Feat: Add latest tag to push Docker Hub

--- web_scraping/microservice_get_users/production.Unix.sh
+++ web_scraping/microservice_get_users/production.Unix.sh
@@ -12,3 +12,7 @@ docker build --no-cache --platform linux/amd64 -t $DOCKER_MICROSERVICE_GET_USERS
 docker tag $DOCKER_MICROSERVICE_GET_USERS semillerosmart/microservice_get_users:$DOCKER_VERSION_IMG
 
 docker push semillerosmart/microservice_get_users:$DOCKER_VERSION_IMG
+
+# Push latest version
+docker tag $DOCKER_MICROSERVICE_GET_USERS semillerosmart/microservice_get_users:latest
+docker push semillerosmart/microservice_get_users:latest
