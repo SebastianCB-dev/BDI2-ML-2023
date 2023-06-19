2459574457efdc5c91f6433ad47f4a749ae49d88
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-06-19 12:06:53 -0500

    Feat: add scripts

---
+++ web_scraping/microservice_get_users/production.Unix.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+DOCKER_MICROSERVICE_GET_USERS="microservice_get_users"
+
+DOCKER_VERSION_IMG="1.1.0-prod"
+
+# build browser
+docker build --no-cache -t $DOCKER_MICROSERVICE_GET_USERS .
+
+# Push version
+docker tag $DOCKER_MICROSERVICE_GET_USERS semillerosmart/microservice_get_users:$DOCKER_VERSION_IMG
+
+docker push semillerosmart/microservice_get_users:$DOCKER_VERSION_IMG
