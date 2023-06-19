d16f3641dda981ddf92f7f54e23665de76aa4772
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-06-19 14:52:39 -0500

    Feat: Updated production.Unix.sh

--- web_scraping/microservice_get_users/production.Unix.sh
+++ web_scraping/microservice_get_users/production.Unix.sh
@@ -10,7 +10,6 @@ docker build --no-cache --platform linux/amd64 -t $DOCKER_MICROSERVICE_GET_USERS
 
 # Push version
 docker tag $DOCKER_MICROSERVICE_GET_USERS semillerosmart/microservice_get_users:$DOCKER_VERSION_IMG
-
 docker push semillerosmart/microservice_get_users:$DOCKER_VERSION_IMG
 
 # Push latest version
