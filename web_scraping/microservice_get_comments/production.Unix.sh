6e2f62c426826f47e3450744698f8c27c180a91c
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-06-21 19:12:24 -0500

    Update productionUnixsh with latest tag

--- web_scraping/microservice_get_comments/production.Unix.sh
+++ web_scraping/microservice_get_comments/production.Unix.sh
@@ -3,12 +3,15 @@ set -e
 
 DOCKER_MICROSERVICE_GET_COMMENTS="microservice_get_comments"
 
-DOCKER_VERSION_IMG="1.0.0-prod"
+DOCKER_VERSION_IMG="1.1.0-prod"
 
 # build browser
-docker build --no-cache -t $DOCKER_MICROSERVICE_GET_COMMENTS .
+docker build --no-cache --platform linux/amd64 -t $DOCKER_MICROSERVICE_GET_COMMENTS .
 
 # Push version
 docker tag $DOCKER_MICROSERVICE_GET_COMMENTS semillerosmart/microservice_get_comments:$DOCKER_VERSION_IMG
-
 docker push semillerosmart/microservice_get_comments:$DOCKER_VERSION_IMG
+
+# Push latest
+docker tag $DOCKER_MICROSERVICE_GET_COMMENTS semillerosmart/microservice_get_comments:latest
+docker push semillerosmart/microservice_get_comments:latest
