fa90a86fe260c713f2522c16d696654e3d456943
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-07-09 16:36:52 -0500

    Update Version

--- web_scraping/microservice_get_comments/production.Unix.sh
+++ web_scraping/microservice_get_comments/production.Unix.sh
@@ -3,7 +3,7 @@ set -e
 
 DOCKER_MICROSERVICE_GET_COMMENTS="microservice_get_comments"
 
-DOCKER_VERSION_IMG="1.1.0-prod"
+DOCKER_VERSION_IMG="1.2.0-prod"
 
 # build browser
 docker build --no-cache --platform linux/amd64 -t $DOCKER_MICROSERVICE_GET_COMMENTS .
