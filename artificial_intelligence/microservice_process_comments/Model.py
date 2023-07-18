835bdf90ba0ed45f4341370d8d0fd3ceee70c799
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-07-18 18:22:43 -0500

    Feat: add database get user

--- artificial_intelligence/microservice_process_comments/Model.py
+++ artificial_intelligence/microservice_process_comments/Model.py
@@ -1,9 +1,11 @@
 # Standard libraries
 import os
+import time
 
 # Custom libraries
 from services.database_service import DatabaseService
 from services.logging_service import LoggingService
+
 
 class Model:
     # Public Properties
@@ -15,13 +17,13 @@ class Model:
             This function initializes the class
             Set the model and connect to the database
         """
-        #self.set_model()
+        # self.set_model()
         self.start_db()
 
     def getLogger(self):
         # This function returns the logger
         return self.logger
-    
+
     def start_db(self):
         """Database
             This function starts the database connection
@@ -29,3 +31,15 @@ class Model:
         """
         self.databaseService = DatabaseService(os.getenv('POSTGRES_URL'))
         self.logger.info("Database connection started")
+
+    def process_comments(self):
+        while True:
+            try:
+                comments = self.databaseService.get_comments()
+                for comment in comments:
+                    self.logger.info(f"Processing comment: {comment['id']}- {comment['username']}")
+                    bdi = self.self.databaseService.get_bdi(comment['user_comment'])
+                time.sleep(20)
+            except Exception as e:
+                time.sleep(120)
+                continue
--- artificial_intelligence/microservice_process_comments/services/database_service.py
+++ artificial_intelligence/microservice_process_comments/services/database_service.py
@@ -11,3 +11,10 @@ class DatabaseService:
         self.conn = psycopg2.connect(
             url, sslmode='require', connect_timeout=10)
         self.cur = self.conn.cursor()
+
+    def get_comments(self):
+        # This function returns the comments
+        self.cur.execute("SELECT id, username, user_comment FROM comments WHERE comment_status = 'PENDING'")
+        comments = self.cur.fetchall()
+        comments = [{'id': comment[0], 'username': comment[1], 'user_comment': comment[2]} for comment in comments]        
+        return comments
