a7623c9e7ee4e7b3a381c1bb2941855aea04f0a8
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-11-04 18:02:06 -0500

    Add: functions to add users and validate if it exists into the database

--- web_scraping_node/microservice_get_users/src/classes/Database.ts
+++ web_scraping_node/microservice_get_users/src/classes/Database.ts
@@ -1,32 +1,49 @@
-import { Pool } from 'pg'
-import { Logger } from './Logger'
+import {Pool} from 'pg'
+import {Logger} from './Logger'
+import {User} from '../interface/User'
 
 export class Database {
 
-  pool: Pool | undefined = undefined
+  private _pool: Pool | undefined = undefined
 
-  constructor () {
-    this.createConnection()
+  constructor() {
+    this.createConnection ()
   }
 
-  createConnection (): void {
-    this.pool = new Pool({
+  createConnection(): void {
+    this._pool = new Pool ({
       connectionString: process.env.POSTGRES_URL,
     })
     // Validate connection
-    // Attempt a test query
-    this.pool.connect()
-      .then(() => {
-        Logger.infoLog('✅ Database connection established')
+    this._pool.connect ()
+      .then (() => {
+        Logger.infoLog ('✅ Database connection established')
       })
-      .catch((err) => {
-        Logger.errorLog('❌ Database connection failed')
-        Logger.errorLog(err as string)
-        throw new Error('Error when connecting to the database')
+      .catch ((err) => {
+        Logger.errorLog ('❌ Database connection failed')
+        Logger.errorLog (err as string)
+        throw new Error ('Error when connecting to the database')
       })
   }
 
-  getPool () {
-    return this.pool
+  async addUsers(users: User[]) {
+    for (let i = 0; i < users.length; i++) {
+      const {username, fullName} = users[i]
+      const existsUser = await this.existUserInDatabase (username)
+      if (!existsUser) {
+        try {
+          await this._pool!.query ('INSERT INTO users (username, fullname) VALUES ($1, $2)', [username, fullName])
+          Logger.infoLog (`✅ User ${username} added to database`)
+        } catch (err) {
+          Logger.errorLog (`❌ Error when adding user ${username} to database`)
+          Logger.errorLog (err as string)
+        }
+      }
+    }
+  }
+
+  async existUserInDatabase(username: string): Promise<boolean> {
+    const res = await this._pool!.query ('SELECT username FROM users WHERE username = $1', [username])
+    return (res.rows.length > 0)
   }
 }
